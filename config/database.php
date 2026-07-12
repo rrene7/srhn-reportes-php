888c87c34f37ff288f0b764aecbb3f54aa5b59d4
rrene7 <rrenys.evang0907@gmail.com>
2026-07-12 11:12:04 -0500

    Configurar base principal y base RRHH

--- config/database.php
+++ config/database.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 use App\Support\Env;
 
+$defaultHost = Env::get('DB_HOST', '127.0.0.1');
+$defaultPort = Env::get('DB_PORT', '3306');
+$defaultUser = Env::get('DB_USER', 'root');
+$defaultPass = Env::get('DB_PASS', '');
+$defaultCharset = Env::get('DB_CHARSET', 'utf8mb4');
+
 return [
-    'host' => Env::get('DB_HOST', '127.0.0.1'),
-    'port' => Env::get('DB_PORT', '3306'),
-    'database' => Env::get('DB_NAME', 'srhn'),
-    'username' => Env::get('DB_USER', 'root'),
-    'password' => Env::get('DB_PASS', ''),
-    'charset' => Env::get('DB_CHARSET', 'utf8mb4'),
+    'default' => [
+        'host' => $defaultHost,
+        'port' => $defaultPort,
+        'database' => Env::get('DB_NAME', 'rhhgith'),
+        'username' => $defaultUser,
+        'password' => $defaultPass,
+        'charset' => $defaultCharset,
+    ],
+    'rrhh' => [
+        'host' => Env::get('RRHH_DB_HOST', $defaultHost),
+        'port' => Env::get('RRHH_DB_PORT', $defaultPort),
+        'database' => Env::get('RRHH_DB_NAME', 'rrhh2029'),
+        'username' => Env::get('RRHH_DB_USER', $defaultUser),
+        'password' => Env::get('RRHH_DB_PASS', $defaultPass),
+        'charset' => Env::get('RRHH_DB_CHARSET', $defaultCharset),
+    ],
 ];
